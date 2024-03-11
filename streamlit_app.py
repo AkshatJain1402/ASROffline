3c8b49269ce502109df0a602e35e1f704fcfef41
AkshatJain1402 <akshatjain2vb5@gmail.com>
2024-03-12 00:54:15 +0530

    added ui

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,30 +1,37 @@
 #start
 import streamlit as st
+col1,col2 ,col3=st.columns(3)
+
+with st.container():
+    
+    st.title("Customer Care")
+    st.image("UI.jpg", caption=None, width=None, use_column_width=None, clamp=False, channels="RGB", output_format="auto")
+    with col1:
+        st.header("streaming ASR")
+        st.image("https://static.streamlit.io/examples/cat.jpg")
+
+        
+        clicked=st.button("Click me for translation",key="my_btn")
+    with col2:
+        st.header("offline ASR")
+        st.file_uploader("upload your recording ", type=None, accept_multiple_files=False, key=None,  on_change=None, args=None, kwargs=None, disabled=False, label_visibility="visible")
+        clicked=st.button("Click me for translation",key="my_btn")
+    with col3:
+        st.header('Results')
+        text_contents="the results "
+        st.write(text_contents)
+
+        st.download_button("your transcription",text_contents, file_name=None, mime=None, key=None, help=None, on_click=None, args=None, kwargs=None, type="secondary", disabled=False, use_container_width=False)
 
 
 
-from streamlit_webrtc import webrtc_streamer, WebRtcMode
-def record_audio():
-    with st.spinner("Recording..."):
-        audio_recorder = webrtc_streamer(
-            key="audio-recorder",
-            mode=WebRtcMode.AUDIO_RECVONLY,  # Receive audio only 
-            audio_receiver_size=256,  # Adjust as needed
-            rtc_configuration={"iceServers": [{"urls": ["stun:stun.l.google.com:19302"]}]} 
-        )
 
-        while True:
-            if audio_recorder.audio_receiver:
-                sound_bytes = audio_recorder.audio_receiver.get_frames(timeout=5)
-                break  # Exit the loop after recording some audio
+       
 
-    st.success("Recording complete!")
-    return sound_bytes  
 
-# Record Button
-if st.button("Record Voice"):
-    sound_bytes = record_audio()
 
-    # Further processing (Save the audio, display waveform, etc.)
-    if sound_bytes:
-        st.audio(sound_bytes, format="audio/wav")
+
+
+
+
+
